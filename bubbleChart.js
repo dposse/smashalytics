cd5e2b9635162a539f97ac7026c6076cfa4a4f19
dposse <dposse.125@gmail.com>
2019-02-16 23:30:24 -0500

    Created main.js to load data and call bubbleChart.js

--- bubbleChart.js
+++ bubbleChart.js
@@ -1,11 +1,3 @@
 /*
 * Creates bubble chart with D3.js
 */
-
-d3.csv("tournamentData.csv", (data) => {
-
-  console.log(data);
-
-
-
-});
---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+* Load data from csv and create charts
+*/
+
+d3.csv("tournamentData.csv", (data) => {
+
+  console.log(data);
+
+});
