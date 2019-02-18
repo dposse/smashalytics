87be32b85014f75ba1bfa789aa305fb05b838e1b
dposse <dposse.125@gmail.com>
2019-02-18 14:25:05 -0500

    Node radius correlates to data.entrants

--- bubbleChart.js
+++ bubbleChart.js
@@ -40,7 +40,7 @@ d3.csv("tournamentData.csv")
        .append("circle")
        .attr("fill", "turquoise")
        .attr("stroke", "black")
-       .attr("r", 25)
+       .attr("r", (data) => { return data.entrants / 7; })
        .attr("cx", (d) => { return Math.random() * parseInt(svg.style("width")); })
        .attr("cy", (d) => { return Math.random() * parseInt(svg.style("height")); });
 
