5eb9dd7ff19a152b211020ca47f7f3926d1368ae
dposse <dposse.125@gmail.com>
2019-05-22 16:23:34 -0400

    Read csv with fast-csv

--- scrapeMatchData.js
+++ scrapeMatchData.js
@@ -9,43 +9,14 @@
 // import modules
 const axios = require('axios');
 const fs    = require('fs');
-const parse = require('csv-parse');
+const csv   = require('fast-csv');
 
-let parser = parse({delimiter: ','}, function(err, data) {
-    console.log(data);
-});
+let urls = [];
 
-fs.createReadStream('tournamentData.csv').pipe(parser);
-
-
-
-/*
-*   original code to scrape brackets from array of urls
-
-function scrapeBrackets() {
-  
-    //find all urls to scrape
-    let urls = tournamentData.map( tournament => tournament.url );
-  
-    
-    
-    return new Promise( (resolve, reject) => {
-  
-        request("https://www.ssbwiki.com/Tournament:Sky%27s_Ultimate_Invitational", function(error, response,body) {
-  
-          if (!error && response.statusCode === 200) {
-  
-            $ = cheerio.load(body);
-  
-            
-  
-            resolve();
-  
-          } //end if
-  
-          else { reject(); }
-  
-        }); //end request
-  
-    }); //end promise
-  } //end scrapeBrackets*/
+csv.fromPath('tournamentData.csv', {headers: true})
+    .on('data', data => {
+        console.log(data);
+    })
+    .on('end', () => {
+        console.log('hey im done');
+    });
